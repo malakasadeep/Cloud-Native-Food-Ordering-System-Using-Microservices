322c3170f8971fe7e8bd84c2416619aa99a1c01b
Dewni Jayasuriya <dewnijayasuriya123@gmail.com>
2025-04-13 20:25:46 +0530

    create order schema

---
+++ backend/order-service/models/orderModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    customerId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    items: [
+      {
+        itemId: { type: mongoose.Schema.Types.ObjectId, required: true },
+        itemName: { type: String, required: true },
+        unitPrice: { type: Number, required: true },
+        qty: { type: Number, required: true },
+      },
+    ],
+    totalAmount: { type: Number, required: true },
+    paymentMethod: { type: String, enum: ["Cash", "Card"], required: true },
+    cardInfo: {
+      cardNumber: { type: String },
+      expiryDate: { type: String },
+      cvv: { type: String },
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["PENDING", "PAID"],
+      default: "PENDING",
+      required: false,
+    },
+    orderStatus: {
+      type: String,
+      enum: ["PLACED", "PROCESSING", "COMPLETED", "CANCELLED"],
+      default: "PLACED",
+    },
+    deliveryAddress: { type: String, required: true },
+    paymentId: { type: String },
+    notificationSent: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
