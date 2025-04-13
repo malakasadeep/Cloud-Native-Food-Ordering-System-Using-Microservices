6846768a27e0500fd84440a717535d0b1f27d80c
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2025-04-13 23:54:32 +0530

    Refactor user service: replace authController with customerController, implement OTP functionality, and update routes

--- backend/user-service/routes/customerRoute.js
+++ backend/user-service/routes/customerRoute.js
@@ -1,15 +1,18 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import * as customerController from '../controllers/customerController.js';
-import { authenticate } from '../middlewares/authMiddleware.js';
+import * as customerController from "../controllers/customerController.js";
+import { authenticate } from "../middlewares/authMiddleware.js";
 
-router.post('/send-mobile-otp', customerController.sendMobileOTP);
-router.post('/verify-mobile-otp', customerController.verifyMobileOTP);
-router.post('/send-email-otp', customerController.sendEmailOTP);
-router.post('/verify-email-otp', customerController.verifyEmailOTP);
-router.get('/signout', customerController.signOut);
+router.post("/send-mobile-otp", customerController.sendMobileOTP);
+router.post("/verify-mobile-otp", customerController.verifyMobileOTP);
+router.post("/send-email-otp", customerController.sendEmailOTP);
+router.post("/verify-email-otp", customerController.verifyEmailOTP);
+router.get("/signout", customerController.signOut);
 //router.post('/google-login', authController.googleLogin);
-router.post('/complete-profile', authenticate, customerController.completeProfile);
-
+router.post(
+  "/complete-profile",
+  authenticate,
+  customerController.completeProfile
+);
 
 export default router;
