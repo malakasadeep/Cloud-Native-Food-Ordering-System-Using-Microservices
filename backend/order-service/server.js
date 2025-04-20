c879bb60fa19221a27d2a7a8e66dacd001949b2a
Dewni Jayasuriya <dewnijayasuriya123@gmail.com>
2025-04-20 12:05:45 +0530

    create server.js

---
+++ backend/order-service/server.js
@@ -0,0 +1,44 @@
+import dotenv from "dotenv";
+dotenv.config();
+import dbConnect from "./config/dbConnect.js";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import http from "http";
+import orderRoutes from "./routes/orderRoute.js";
+import cartRoutes from "./routes/cartRoute.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+const DEFAULT_PORT = process.env.PORT || 5002;
+const FALLBACK_PORT = 7002;
+
+app.use("/api/orders", orderRoutes);
+app.use("/api/cart", cartRoutes);
+
+const startServer = (port) => {
+  const server = http.createServer(app);
+
+  server.listen(port, () => {
+    console.log(`Order Service running on port ${port}`);
+  });
+
+  server.on("error", (err) => {
+    if (err.code === "EADDRINUSE") {
+      console.log(
+        `Port ${port} is already in use. Trying port ${FALLBACK_PORT}...`
+      );
+      startServer(FALLBACK_PORT); // Retry with fallback port
+    } else {
+      console.error("Server error:", err);
+    }
+  });
+};
+
+dbConnect().then(() => {
+  startServer(DEFAULT_PORT);
+});
