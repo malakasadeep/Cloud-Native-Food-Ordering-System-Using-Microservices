cf1a6159c1738f6f740511412685a12f264e3055
iamsahan <weerakkodyse@gmail.com>
2025-04-22 18:11:36 +0530

    config auto assignment logic

--- backend/delivery-service/src/controllers/DeliveryController.js
+++ backend/delivery-service/src/controllers/DeliveryController.js
@@ -1,6 +1,21 @@
 //controllers/DeliveryController.js
 const Delivery = require("../models/Delivery.js");
+const axios = require("axios");
 
 class DeliveryController {
-  static async createNewDelivery(req, res, next) {}
+  static async startToDelivery(req, res, next) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(404).json({ message: "Order Id is Required" });
+      }
+
+      const orderRes = await axios.get(
+        `http://localhost:5002/api/orders/${id}`
+      );
+
+      console.log(orderRes); //loger for debugging
+    } catch (error) {}
+  }
 }
