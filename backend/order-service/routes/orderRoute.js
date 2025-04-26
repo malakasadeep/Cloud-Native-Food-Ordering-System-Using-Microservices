524a133b26f919693dbf5f73de03262a1028c334
Dewni Jayasuriya <dewnijayasuriya123@gmail.com>
2025-04-27 00:10:16 +0530

    added create-checkout-session route path

--- backend/order-service/routes/orderRoute.js
+++ backend/order-service/routes/orderRoute.js
@@ -3,12 +3,15 @@ import {
   placeOrder,
   updateOrder,
   cancelOrder,
-  trackOrder
+  trackOrder,
+  createPaymentIntent
 } from "../controllers/orderController.js"; // Make sure this is also an ES module
 
 const router = express.Router();
 
+
 router.post("/", placeOrder);             // Place Order
+router.post("/create-checkout-session", createPaymentIntent);             // Place Order
 router.put("/:orderId", updateOrder);     // Update Order
 router.delete("/:orderId", cancelOrder);  // Cancel Order
 router.get("/:orderId", trackOrder);      // Track Order
