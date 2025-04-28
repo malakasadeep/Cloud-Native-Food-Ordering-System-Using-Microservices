0ac01350916a8930bf69704a89c21e5703ced3ad
Dewni Jayasuriya <dewnijayasuriya123@gmail.com>
2025-04-28 20:32:39 +0530

    changes

--- backend/order-service/config/dbConnect.js
+++ backend/order-service/config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnect = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL); // Options not needed anymore
+    const conn = await mongoose.connect(process.env.MONGODB_URL); 
     console.log(
       `MongoDB connected: ${conn.connection.host}, ${conn.connection.name}`
     );
