f31f16847673eb05e3aaf0dfcd088d30e79d2cfe
Dewni Jayasuriya <dewnijayasuriya123@gmail.com>
2025-04-29 22:01:29 +0530

    added routes

--- backend/order-service/routes/orderRoute.js
+++ backend/order-service/routes/orderRoute.js
@@ -5,17 +5,20 @@ import {
   cancelOrder,
   trackOrder,
   createPaymentIntent,
-  getAllOrders
+  getAllOrders,
+  updateOrderStatus,
+  getOrderById,
 } from "../controllers/orderController.js"; // Make sure this is also an ES module
 
 const router = express.Router();
 
-
-router.post("/", placeOrder);             // Place Order
-router.post("/create-checkout-session", createPaymentIntent);             // Place Order
+router.post("/", placeOrder); // Place Order
+router.post("/create-checkout-session", createPaymentIntent); // Place Order
 router.get("/", getAllOrders); // Get All Orders
-router.put("/:orderId", updateOrder);     // Update Order
-router.delete("/:orderId", cancelOrder);  // Cancel Order
-router.get("/:orderId", trackOrder);      // Track Order
+router.post("/status/:orderId", updateOrderStatus); // Update Order Status
+router.get("/:id", getOrderById); // Get Order by ID
+router.put("/:orderId", updateOrder); // Update Order
+router.delete("/:orderId", cancelOrder); // Cancel Order
+router.get("/:orderId", trackOrder); // Track Order
 
-export default router; 
+export default router;
